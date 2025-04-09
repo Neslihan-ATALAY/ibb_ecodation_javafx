f810e0dd68fa05db7222af089e8e8954a0ede941
Neslihan ATALAY <118984486+Neslihan-ATALAY@users.noreply.github.com>
2025-04-10 00:38:39 +0300

    Update HelloController.java
    login user id

--- src/main/java/com/neslihanatalay/ibb_ecodation_javafx/HelloController.java
+++ src/main/java/com/neslihanatalay/ibb_ecodation_javafx/HelloController.java
@@ -8,30 +8,30 @@ import org.apache.commons.*
 import org.apache.commons.httpclient.*;
 
 public class HelloController {
-	private UserDAO userDAO;
+    private UserDAO userDAO;
 
     public HelloController() {
         userDAO = new UserDAO();
     }
 
     @FXML private Label welcomeText;
-	@FXML private Label LoginUserIdLabelField;
-	@FXML private Label welcomeLabel;
-	private static Integer loginUserId;
+    @FXML private Label LoginUserIdLabelField;
+    @FXML private Label welcomeLabel;
+    private static Integer loginUserId;
 	
-	public static Integer getLoginUserId() { return loginUserId; }
-	public static void setLoginUserId(Integer loginUserId) { this.loginUserId = loginUserId; }
+    public static Integer getLoginUserId() { return loginUserId; }
+    public static void setLoginUserId(Integer loginUserId) { this.loginUserId = loginUserId; }
 	
-	@FXML
+    @FXML
     public void initialize() {
-		// GİRİŞ YAPAN KULLANICININ ID NUMARASI LoginUserIdLabelField'e KAYDEDİLİR
-		//LoginUserIdLabelField.setText(Integer.valueOf(request.getParameter("user")));
-		setLoginUserId(Integer.valueOf(Request["user"].toString()));
-		LoginUserIdLabelField.setText(Request["user"].toString());
-		UserDTO userDTO = userDAO.findById(getLoginUserId());
-		if (userDTO.isPresent()) {
-			welcomeLabel.setText("Merhaba " + userDTO.getUsername() + ", Hoşgeldiniz");
-		}
+	// GİRİŞ YAPAN KULLANICININ ID NUMARASI LoginUserIdLabelField'e KAYDEDİLİR
+	//LoginUserIdLabelField.setText(Integer.valueOf(request.getParameter("user")));
+	setLoginUserId(Integer.valueOf(Request["user"].toString()));
+	LoginUserIdLabelField.setText(Request["user"].toString());
+	UserDTO userDTO = userDAO.findById(getLoginUserId());
+	if (userDTO.isPresent()) {
+	    welcomeLabel.setText("Merhaba " + userDTO.getUsername() + ", Hoşgeldiniz");
+	}
     }
 
     @FXML
