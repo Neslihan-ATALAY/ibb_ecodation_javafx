db86ae153d6e1a056b44c07efabe24f402809a11
Neslihan ATALAY <118984486+Neslihan-ATALAY@users.noreply.github.com>
2025-04-10 00:28:05 +0300

    Update HelloController.java
    login user id

--- src/main/java/com/neslihanatalay/ibb_ecodation_javafx/HelloController.java
+++ src/main/java/com/neslihanatalay/ibb_ecodation_javafx/HelloController.java
@@ -1,11 +1,38 @@
 package com.neslihanatalay.ibb_ecodation_javafx;
 
+import com.neslihanatalay.ibb_ecodation_javafx.dao.UserDAO;
+import com.neslihanatalay.ibb_ecodation_javafx.dto.UserDTO;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import org.apache.commons.*
+import org.apache.commons.httpclient.*;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
+	private UserDAO userDAO;
+
+    public HelloController() {
+        userDAO = new UserDAO();
+    }
+
+    @FXML private Label welcomeText;
+	@FXML private Label LoginUserIdLabelField;
+	@FXML private Label welcomeLabel;
+	private static Integer loginUserId;
+	
+	public static Integer getLoginUserId() { return loginUserId; }
+	public static void setLoginUserId(Integer loginUserId) { this.loginUserId = loginUserId; }
+	
+	@FXML
+    public void initialize() {
+		// GİRİŞ YAPAN KULLANICININ ID NUMARASI LoginUserIdLabelField'e KAYDEDİLİR
+		//LoginUserIdLabelField.setText(Integer.valueOf(request.getParameter("user")));
+		setLoginUserId(Integer.valueOf(Request["user"].toString()));
+		LoginUserIdLabelField.setText(Request["user"].toString());
+		UserDTO userDTO = userDAO.findById(getLoginUserId());
+		if (userDTO.isPresent()) {
+			welcomeLabel.setText("Merhaba " + userDTO.getUsername() + ", Hoşgeldiniz");
+		}
+    }
 
     @FXML
     protected void onHelloButtonClick() {
