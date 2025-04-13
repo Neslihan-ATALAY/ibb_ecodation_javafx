290bdf171cce7ad4c5bcc5d064d89661a2ffe13d
Neslihan ATALAY <118984486+Neslihan-ATALAY@users.noreply.github.com>
2025-04-13 14:51:19 +0300

    Update FXMLPath.java
    TR ve EN fxml view sayfalar

--- src/main/java/com/neslihanatalay/ibb_ecodation_javafx/utils/FXMLPath.java
+++ src/main/java/com/neslihanatalay/ibb_ecodation_javafx/utils/FXMLPath.java
@@ -2,9 +2,15 @@ package com.neslihanatalay.ibb_ecodation_javafx.utils;
 
 public class FXMLPath {
 
-    public static final String LOGIN = "/com/neslihanatalay/ibb_ecodation_javafx/view/login.fxml";
-    public static final String REGISTER = "/com/neslihanatalay/ibb_ecodation_javafx/view/register.fxml";
-    public static final String ADMIN = "/com/neslihanatalay/ibb_ecodation_javafx/view/admin.fxml";
-    public static final String USER_HOME = "/com/neslihanatalay/ibb_ecodation_javafx/view/hello-view.fxml";
-    public static final String NOTEBOOK = "/com/neslihanatalay/ibb_ecodation_javafx/view/notebook.fxml";
+    public static final String LOGIN_TR = "/com/neslihanatalay/ibb_ecodation_javafx/view/loginTR.fxml";
+	public static final String LOGIN_EN = "/com/neslihanatalay/ibb_ecodation_javafx/view/loginEN.fxml";
+    public static final String REGISTER_TR = "/com/neslihanatalay/ibb_ecodation_javafx/view/registerTR.fxml";
+	public static final String REGISTER_EN = "/com/neslihanatalay/ibb_ecodation_javafx/view/registerEN.fxml";
+    public static final String ADMIN_TR = "/com/neslihanatalay/ibb_ecodation_javafx/view/adminTR.fxml";
+	public static final String ADMIN_EN = "/com/neslihanatalay/ibb_ecodation_javafx/view/adminEN.fxml";
+    public static final String USER_HOME_TR = "/com/neslihanatalay/ibb_ecodation_javafx/view/hello-viewTR.fxml";
+	public static final String USER_HOME_EN = "/com/neslihanatalay/ibb_ecodation_javafx/view/hello-viewEN.fxml";
+	public static final String NOTEBOOK_TR = "/com/neslihanatalay/ibb_ecodation_javafx/view/notebookTR.fxml";
+	public static final String NOTEBOOK_EN = "/com/neslihanatalay/ibb_ecodation_javafx/view/notebookEN.fxml";
+
 }
